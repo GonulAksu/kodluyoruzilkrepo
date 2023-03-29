dcb5114504b5c40d382a769f1c99956c09ec5a05
Gönül Aksu <aks766gnl@hotmail.com>
2023-03-29 19:33:00 +0300

    son

---
+++ React/Odev1/app.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+export default async(userid)=>{
+        const {data:user}=await axios(`https://jsonplaceholder.typicode.com/users/${userid}`);
+        
+
+        const {data:posts}=await axios(`https://jsonplaceholder.typicode.com/posts?userId=${userid}`);
+
+      console.log({"User":user,"Posts":posts})
+}
+
+//2.yöntem
+
+//  const data=(userid)=>{
+//     return new Promise(async (resolve,reject)=>{
+//         if(typeof(userid) ==='number'){
+//             const {data:user}=await axios(`https://jsonplaceholder.typicode.com/users/${userid}`);
+            
+//             const {data:posts}=await axios(`https://jsonplaceholder.typicode.com/posts?userId=${userid}`)
+//             resolve({"User":user,"Posts":posts});
+//         }
+//         else{
+//             reject("userid Number değer olmalı.");
+//         }
+//     })
+// }
+
+//  export default async()=>{
+//     try{
+//         const lastdata=await data(1);
+//         console.log(lastdata);
+//     }
+//     catch(e){
+//         console.log(e);
+//     }
+// }
