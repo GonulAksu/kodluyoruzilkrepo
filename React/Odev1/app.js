b2f0ebf815cf14c15b2875d664276d4b33e8f812
Gönül Aksu <aks766gnl@hotmail.com>
2023-03-30 19:47:48 +0300

    trycatch

--- React/Odev1/app.js
+++ React/Odev1/app.js
@@ -1,38 +1,17 @@
 import axios from "axios";
 
 export default async(userid)=>{
-        const {data:user}=await axios(`https://jsonplaceholder.typicode.com/users/${userid}`);
+  try{
+        const {data:user}=await axios(`https://jsonplaceholder.typicode.com/users/${userid}`); //get user
         
 
-        const {data:posts}=await axios(`https://jsonplaceholder.typicode.com/posts?userId=${userid}`);
+        const {data:posts}=await axios(`https://jsonplaceholder.typicode.com/posts?userId=${userid}`); //get posts for user
 
-      // console.log({"User":user,"Posts":posts})
       return {...user, posts:posts };
+  }catch(e){
+    return "userid Number must be a value"
+  }
 }
 
-//2.yöntem
 
-//  const data=(userid)=>{
-//     return new Promise(async (resolve,reject)=>{
-//         if(typeof(userid) ==='number'){
-//             const {data:user}=await axios(`https://jsonplaceholder.typicode.com/users/${userid}`);
-            
-//             const {data:posts}=await axios(`https://jsonplaceholder.typicode.com/posts?userId=${userid}`)
-//             resolve({"User":user,"Posts":posts});
-//         }
-//         else{
-//             reject("userid Number değer olmalı.");
-//         }
-//     })
-// }
-
-//  export default async()=>{
-//     try{
-//         const lastdata=await data(1);
-//         console.log(lastdata);
-//     }
-//     catch(e){
-//         console.log(e);
-//     }
-// }
 
--- React/Odev1/index.js
+++ React/Odev1/index.js
@@ -4,6 +4,5 @@ import getData from './app.js';
     const result = await getData(1); 
     console.log(result);
   })();
-// getData(1);
-// getData();
+
 
